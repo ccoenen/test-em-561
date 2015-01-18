a942c794c2e7298ff24e7f8ffa98f7e6213eb23e
Claudius Coenen <opensource@amenthes.de>
2015-01-18 17:44:36 +0100

    empty vanilla rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emtest::Application.config.secret_token = '18b80bab24230f100715731cd55311fce2d487c25375d4a6d853b813ea7e3b377a306aa41d6854734b37e17d53dc665ec7852582fd9a36f80c99392930903da7'
